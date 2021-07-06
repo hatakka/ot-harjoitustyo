28f449bb6e529fadffc79a608ce3ecc933a6f400
Taina Hatakka <taina.hatakka@kotiposti.net>
2021-07-06 16:48:04 +0300

    osa1: 1.3

--- laskarit/osa1/src/App.js
+++ laskarit/osa1/src/App.js
@@ -13,9 +13,9 @@ const Content = (props) => {
   console.log(props);
   return(
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.p1}/>
+      <Part part={props.p2}/>
+      <Part part={props.p3}/>
     </div>
   );
 }
@@ -32,7 +32,7 @@ const Part = (props) => {
   return(
     <>
       <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
   );
@@ -40,18 +40,23 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total count={exercises1+exercises2+exercises3} />
+      <Content p1={part1} p2={part2} p3={part3} />
+      <Total count={part1.exercises+part2.exercises+part3.exercises} />
     </div>
   );
 }
