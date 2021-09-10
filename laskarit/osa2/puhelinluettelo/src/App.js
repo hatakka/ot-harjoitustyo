0b9ce07efa5122fcfd93132bebef00a43338b403
Taina Hatakka <taina.hatakka@kotiposti.net>
2021-09-10 17:32:31 +0300

    2.16: puhelinluettelo step8

--- laskarit/osa2/puhelinluettelo/src/App.js
+++ laskarit/osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -13,14 +12,13 @@ const App = () => {
   const [filterNumbers, setFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  }, [])
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+    })
+   }, [])
+
   console.log('render', persons.length, 'persons')
 
   const filterChange = (event) => {
@@ -30,31 +28,24 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    let pos = -1
+    const person = persons.find(person => person.name === newName)
 
-    persons.forEach(function(item, index, array) {
-      if (item.name === newName) {
-        pos = index
-      }
-    })
-    if (pos >= 0) {
+    if (person != null) {
       alert(`${newName} is already added to phonebook`)
     } else {
       const personObject = {
         name: newName,
-        number: newNumber,
-        id: persons.length + 1,
+        number: newNumber
       }
-   
-      axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        console.log(response)
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
+
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
         })
-     }
+    }
   }
 
   const handleNameChange = (event) => {
