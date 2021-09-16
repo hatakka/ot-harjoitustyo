bf3ac811da5166328c6914cc31e1adbd5ba076d0
Taina Hatakka <taina.hatakka@kotiposti.net>
2021-09-16 18:34:55 +0300

    3.2: puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (req, res) => {
+  res.send(`<div>Phonebook has info for ${persons.length} people</div><br/>${new Date()}`)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
